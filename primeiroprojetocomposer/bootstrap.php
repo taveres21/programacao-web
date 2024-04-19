28dcb120274d722f12eff4925682682b6e3969ce
Guilherme Tavares <149025209+taveres21@users.noreply.github.com>
2024-04-18 21:41:57 -0300

    Add files via upload

--- primeiroprojetocomposer/bootstrap.php
+++ primeiroprojetocomposer/bootstrap.php
@@ -23,11 +23,35 @@ $r->get('/exer1/formulario',function() {
 });
 
 $r->post('/exer1/resposta',function(){
-    $valor = $_POST['valor1'];
+    $valor1 = $_POST['valor1'];
     $valor2 = $_POST['valor2'];
     $soma = $valor1 + $valor2;
     return "A soma é: {$soma}";
 });
+
+$r->post('/exer2/resposta',function() {
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $valor3 = $_POST['valor3'];
+    $valor4 = $_POST['valor4'];
+    $valor5 = $_POST['valor5'];
+    $valor6 = $_POST['valor6'];
+    $valor7 = $_POST['valor7'];
+
+    $valores = array();
+    
+    array_push($valores, $valor1);
+    array_push($valores, $valor2);
+    array_push($valores, $valor3);
+    array_push($valores, $valor4);
+    array_push($valores, $valor5);
+    array_push($valores, $valor6);
+    array_push($valores, $valor7);
+
+});
+
+
+
 
 #ROTAS
 
