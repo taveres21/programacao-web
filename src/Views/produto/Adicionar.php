2ac4d5107d815a47c88d6695e262187a4ef44857
Guilherme Tavares <149025209+taveres21@users.noreply.github.com>
2024-04-19 06:20:52 -0300

    Add files via upload

---
+++ src/Views/produto/Adicionar.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Inserir Produto</title>
+  </head>
+  <body>
+      <a class="btn btn-secondary mt-3 ms-3" href="/">Inicio</a>
+      <main class="container">
+        <h1>Inserir Produto</h1>
+        <form action="/curso/novo" method="post">
+            <div class="row">
+                <div class="col-6">
+                    <label for="nome" class="form-label">Descrição:</label>
+                    <input type="text" name="nome" class="form-control" required>
+                </div>
+                <div class="col-6">
+                    <label for="peso" class="form-label">Peso:</label>
+                    <input type="text" name="peso" class="form-control" required>
+                </div>
+                <div class="col-6">
+                    <label for="situacao" class="form-label">Situacao:</label>
+                    <input type="text" name="situacao" class="form-control" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="mt-4 btn btn-primary ">Salvar</button>
+                </div>
+            </div>
+        </form>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
--- src/Views/produto/Index.php
+++ src/Views/produto/Index.php
@@ -17,24 +17,24 @@ if (isset($_GET['sucesso'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Professores</title>
+    <title>Produto</title>
 </head>
 
 <body>
 <a class="btn btn-secondary mt-3 ms-3" href="/">Inicio</a>
     <div class="container">
-        <h1>Professores</h1>
+        <h1>produto</h1>
         <div class="d-flex justify-content-end">
-        <a href="/professor/inserir" class="d-flex text-center btn-lg btn btn-primary">Novo Professor</a>
+        <a href="/produto/inserir" class="d-flex text-center btn-lg btn btn-primary">Novo Produto</a>
         </div>
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">Id</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Cpf</th>
-                    <th scope="col">Carga horária</th>
-                    <th scope="col">Formação</th>
+                    <th scope="col">Descricao</th>
+                    <th scope="col">Peso</th>
+                    <th scope="col">Situacao</th>
+                    <!-- <th scope="col">Ações</th> -->
                 </tr>
             </thead>
             <tbody>
@@ -43,10 +43,9 @@ if (isset($_GET['sucesso'])) {
                     ?>
                     <tr>
                         <td><?= $c['id'] ?></td>
-                        <td><?= $c['nome'] ?></td>
-                        <td><?= $c['cpf'] ?></td>
-                        <td><?= $c['carga_horaria'] ?></td>
-                        <td><?= $c['formacao'] ?></td>
+                        <td><?= $c['descricao'] ?></td>
+                        <td><?= $c['peso'] ?></td>
+                        <td><?= $c['situacao'] ?></td>
                     </tr>
                     <?php
                 }
