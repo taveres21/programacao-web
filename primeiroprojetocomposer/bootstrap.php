68fda0647427ecf5f05658a6041b47a0048f0e86
Guilherme Tavares <149025209+taveres21@users.noreply.github.com>
2024-04-19 15:59:15 -0300

    Add files via upload

--- primeiroprojetocomposer/bootstrap.php
+++ primeiroprojetocomposer/bootstrap.php
@@ -9,45 +9,185 @@ $r = new Php\Primeiroprojeto\Router($metodo, $caminho);
 
 #ROTAS
 
-$r->get('/olamundo', function (){
-    return "Olá mundo!";
-} );
-
-$r->get('/olapessoa/{nome}', function($params){
-    return 'Olá '.$params[1];
-} );
-
-$r->get('/exer1/formulario',function() {
-    include('exer1.html');
-
+r->get('/', function () {
+    include ("home.html");
 });
 
-$r->post('/exer1/resposta',function(){
+$r->get('/olamundo', function () {
+    return "Olá mundo!";
+});
+
+$r->get('/olapessoa/{nome}', function ($params) {
+    return 'Olá ' . $params[1];
+});
+
+$r->get('/exemplo1/formulario', function () {
+    include ("exemplo1.html");
+});
+
+$r->post('/exemplo1/resposta', function () {
     $valor1 = $_POST['valor1'];
     $valor2 = $_POST['valor2'];
     $soma = $valor1 + $valor2;
     return "A soma é: {$soma}";
 });
 
-$r->post('/exer2/resposta',function() {
-    $valor1 = $_POST['valor1'];
-    $valor2 = $_POST['valor2'];
-    $valor3 = $_POST['valor3'];
-    $valor4 = $_POST['valor4'];
-    $valor5 = $_POST['valor5'];
-    $valor6 = $_POST['valor6'];
-    $valor7 = $_POST['valor7'];
+$r->get('/exer1/form', function () {
+    include ("listaExercicios/exercicio1.html");
+});
 
-    $valores = array();
-    
-    array_push($valores, $valor1);
-    array_push($valores, $valor2);
-    array_push($valores, $valor3);
-    array_push($valores, $valor4);
-    array_push($valores, $valor5);
-    array_push($valores, $valor6);
-    array_push($valores, $valor7);
+$r->post('/exercicio1/resposta', function () {
+    $num = $_POST['num'];
 
+    if ($num < 0) {
+        return "Valor negativo.";
+    } else if ($num == 0) {
+        return "Igual a zero.";
+    } else {
+        return "Valor positivo";
+    }
+
+});
+
+
+
+$r->get('/exer3/form', function () {
+    include ("listaExercicios/exercicio3.html");
+});
+
+$r->post('/exer3/resposta', function () {
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+
+    $resposta = $num1 + $num2;
+
+    if ($num1 == $num2) {
+        return $resposta * 3;
+    } else {
+        return $resposta;
+    }
+
+});
+
+$r->get('/exer4/form', function () {
+    include ("listaExercicios/exercicio4.html");
+});
+
+$r->post('/exer4/resposta', function () {
+    $num = $_POST['num'];
+
+    for ($x = 1; $x <= 10; $x++) {
+        echo "<h1>" . $num . "x" . $x . " = " . ($num * $x) . "</h1>";
+    }
+});
+
+$r->get('/exer5/form', function () {
+    include ("listaExercicios/exercicio5.html");
+});
+
+$r->post('/exer5/resposta', function () {
+    $num = $_POST['num'];
+    $resposta = 1;
+    if ($num == 0) {
+        return $resposta;
+    } else {
+        for ($num; $num > 1; $num--) {
+            $resposta = $resposta * $num;
+        }
+        return $resposta;
+    };
+});
+
+$r->get('/exer6/form', function () {
+    include ("listaExercicios/exercicio6.html");
+});
+
+$r->post('/exer6/resposta', function () {
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+
+
+
+    if ($num1 == $num2) {
+        return "Números iguais a " . $num1;
+    } else {
+        if ($num1 > $num2) {
+            return $num2 . " " . $num1;
+        } else {
+            return $num1 . " " . $num2;
+        }
+    }
+
+});
+
+$r->get('/exer7/form', function () {
+    include ("listaExercicios/exercicio7.html");
+});
+
+$r->post('/exer7/resposta', function () {
+    $num = $_POST['num'];
+    if ($num <= 0) {
+        return "Insíra um valor válido.";
+    } else {
+        return $num * 100;
+    };
+});
+
+$r->get('/exer8/form', function () {
+    include ("listaExercicios/exercicio8.html");
+});
+
+$r->post('/exer8/resposta', function () {
+    $parede = $_POST['num'];
+    if ($tamParede <= 0) {
+        return "Insíra um valor válido.";
+    } else {
+        $litros = $parede / 3;
+        $latas = $litros / 18;
+        $preco = $latas * 80.00;
+        return "Precisará de " . $latas . " lata(s), sendo igual  R$" . $preco. " reais.";
+    };
+});
+
+
+$r->get('/exer9/form', function () {
+    include ("listaExercicios/exercicio9.html");
+});
+
+$r->post('/exer9/resposta', function () {
+    $anoNasc = $_POST['num'];
+
+    $resposta1 = date('Y') - $anoNasc ;
+    $resposta2 = $resposta1 * 365;
+    $resposta3 = 2025 - $anoNasc; 
+
+    echo "A)  ". $resposta1. " <br>
+          B)  ". $resposta2. " <br>
+          C)  ". $resposta3. " ";
+});
+
+
+$r->get('/exer10/form', function () {
+    include ("listaExercicios/exercicio10.html");
+});
+
+$r->post('/exer10/resposta', function () {
+    $peso = $_POST['peso'];
+    $altura = $_POST['altura'];
+
+    $imc = $peso / ($altura) ** 2;
+
+    if ($imc < 18.5) {
+       return "Imc é " . $imc . " magreza";
+    } elseif ($imc >= 18.5 && $imc <= 24.9) {
+        return "Imc é " . $imc . " normal";
+    } elseif ($imc >= 25.0 && $imc <= 29.9) {
+        return "Imc é " . $imc . ", obesidade grau I";
+    } elseif ($imc >= 30.0 && $imc <= 39.9) {
+        return "Imc é " . $imc . ",  obesidade grau II";
+    } elseif ($imc >= 40.0) {
+        return "Imc é " . $imc . ", obesidade grau III";
+    }
 });
 
 
--- primeiroprojetocomposer/vendor/composer/installed.php
+++ primeiroprojetocomposer/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'php/primeiroprojeto',
         'pretty_version' => '1.0.0+no-version-set',
         'version' => '1.0.0.0',
-        'reference' => NULL,
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'php/primeiroprojeto' => array(
             'pretty_version' => '1.0.0+no-version-set',
             'version' => '1.0.0.0',
-            'reference' => NULL,
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
